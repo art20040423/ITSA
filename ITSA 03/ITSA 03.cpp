7f30cc87abb8107135307c5ab68d792fc07d0853
USER <USER@LAPTOP-LSAQEA8E>
2023-03-08 20:48:53 +0800

    加入專案檔案。

---
+++ ITSA 03/ITSA 03.cpp
@@ -0,0 +1,18 @@
+﻿//ITSA：判斷座標是否在圓形的範圍內
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	int r = 100; //圓半徑
+	int x1 = 0, y1 = 0; //圓心座標
+	cout << "請輸入座標："; //重要：測試時沒這行
+	cin >> x >> y;
+	if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= r * r)
+		cout << "inside" << endl;
+	else
+		cout << "outside" << endl;
+
+	return 0;
+}
