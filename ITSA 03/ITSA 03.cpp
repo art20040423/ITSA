137339c372554c803b5e884c72be4922f8d1445b
art20040423 <art20040423@gmail.com>
2023-03-15 16:39:49 +0800

    質數判別(老師版本)

--- ITSA 03/ITSA 03.cpp
+++ ITSA 03/ITSA 03.cpp
@@ -1,15 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
+
+bool isPrime(int p) {
+	//判斷p是否為質數
+	if (p <= 1) return false;
+	for (int i = 2; i < p; i++) {
+		if (p % i == 0) return false;
+	}
+	return true;
+}
+
 int main()
 {
-	int x, y;
-	int r = 100; //圓半徑
-	int x1 = 0, y1 = 0; //圓心座標
-	cin >> x >> y;
-	if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= r * r)
-		cout << "inside" << endl;
-	else
-		cout << "outside" << endl;
+	int n;
+	int primes[100] = { 0 }; //輸入單一數，不需要
+	//int a[3] = { 1,2,3 };
+	//int b[2][3] =
+	//{
+	//	{1,2,3},
+	//	{4,5,6}
+	//};
+	cout << "輸入一個數值n: "; //ITSA不會過
+	cin >> n;
 
-	return 0;
+	//從1開始判斷是不是質數
+	int j = 0;
+	for (int i = 1; i <= n; i++) {
+		if (isPrime(i)) {
+			primes[j] = i;
+			j++;
+		}
+	}
+
+	// foreach loop：輸出
+	cout << "小於等於" << n << "的質數有:" << endl;
+	for (int p : primes) {
+		if (p != 0) cout << p << endl;
+	}
 }
