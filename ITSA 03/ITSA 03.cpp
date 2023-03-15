0d4666c501b08d5bb0018a1280978eff0bd813ce
art20040423 <art20040423@gmail.com>
2023-03-15 14:41:59 +0800

    複數運算

--- ITSA 03/ITSA 03.cpp
+++ ITSA 03/ITSA 03.cpp
@@ -1,15 +1,69 @@
 ﻿#include <iostream>
 using namespace std;
+
+// 複數的類別定義
+class Complex {
+private:
+	int re, im; // 複數的實部與虛部
+public:
+	Complex(int r = 0, int i = 0) {
+		re = r;
+		im = i;
+	}
+
+	Complex operator + (Complex const& obj) {
+		Complex result;
+		result.re = re + obj.re;
+		result.im = im + obj.im;
+		return result;
+	}
+
+	Complex operator - (Complex const& obj) {
+		Complex result;
+		result.re = re - obj.re;
+		result.im = im - obj.im;
+		return result;
+	}
+
+	Complex operator * (Complex const& obj) {
+		Complex result;
+		result.re = re * obj.re - im * obj.im;
+		result.im = re * obj.im + im * obj.re;
+		return result;
+	}
+
+	void display() {
+		cout << re << " " << im << endl;
+	}
+};
+
 int main()
 {
-	int x, y;
-	int r = 100; //圓半徑
-	int x1 = 0, y1 = 0; //圓心座標
-	cin >> x >> y;
-	if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= r * r)
-		cout << "inside" << endl;
-	else
-		cout << "outside" << endl;
+	int n;
+	Complex c[10];
 
-	return 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char oper;
+		int ar, ai, br, bi;
+		cin >> oper >> ar >> ai >> br >> bi;
+		Complex a(ar, ai);
+		Complex b(br, bi);
+
+		switch (oper) {
+		case '+':
+			c[i] = a + b;
+			break;
+		case '-':
+			c[i] = a - b;
+			break;
+		case '*':
+			c[i] = a * b;
+			break;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		c[i].display();
+	}
 }
