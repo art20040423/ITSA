7a0b024fac85acb24d9fbcd495c6019884428a75
art20040423 <art20040423@gmail.com>
2023-04-15 17:27:31 +0800

    最少派車數(初始化)

--- ITSA 03/ITSA 03.cpp
+++ ITSA 03/ITSA 03.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 int main()
 {
-	int x, y;
-	int r = 100; //圓半徑
-	int x1 = 0, y1 = 0; //圓心座標
-	cin >> x >> y;
-	if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= r * r)
-		cout << "inside" << endl;
-	else
-		cout << "outside" << endl;
 
 	return 0;
 }
