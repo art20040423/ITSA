70b551fdcf1af590c7164d1bf1fe539e4f2cc960
art20040423 <art20040423@gmail.com>
2023-04-15 21:46:21 +0800

    最少派車數

--- ITSA 03/ITSA 03.cpp
+++ ITSA 03/ITSA 03.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
+	int n = 0, t[24] = { 0 };//一天24小時 
+	cin >> n;
+	for (int i = 0; i < n; i++)//因為(返回時間恆大於出發時間)，故依輸入時間依序填格子 
+	{
+		int s = 0, d = 0;
+		cin >> s >> d;
+		for (int j = s - 1; j < d - 1; j++)
+		{
+			t[j]++;
+		}
+	}
+	n = t[0];//將不需要的空間再次利用 
+	for (int i = 0; i < 24; i++) { if (n < t[i])n = t[i]; }//尋最大值 
+	cout << n << endl;
 
 	return 0;
 }
