604871bd933c0ef25164f42f476df42626c53d99
art20040423 <art20040423@gmail.com>
2023-04-16 15:24:04 +0800

    計算複利

--- ITSA 03/ITSA 03.cpp
+++ ITSA 03/ITSA 03.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main()
 {
+	long double r, sum;
+	long int n, p;
+	cin >> r;
+	cin >> n;
+	cin >> p;
+
+	sum = 0;
+	/*while (n > 0)
+	{
+		sum += p;
+		sum *= (1.0 + r);
+		n--;
+	}*/
+	for (n; n > 0; n--) {
+		sum += p;
+		sum *= (1.0 + r);
+	}
+
+	cout << (long long)sum << endl;
 
 	return 0;
 }
